bb21679b0342e68094ee5055af07f579ecd5ce6a
Kgo890 <gorekaden501@gmail.com>
2025-04-05 10:15:09 -0700

    some bad results

--- backend/model.py
+++ backend/model.py
@@ -13,12 +13,12 @@ df_true["label"] = 0
 
 # Making both datasets into one DataFrame and only use title
 df = pd.concat([df_fake, df_true], ignore_index=True)
-df["title"] = df["title"].fillna("")
-df = df[["title", "label"]].dropna()
+df["input"] = df["title"].fillna("") + " " + df["text"].fillna("")
+df = df[["input", "label"]]
 
 # Train-test split
 train_texts, temp_texts, train_labels, temp_labels = train_test_split(
-    df["title"].tolist(), df["label"].tolist(), test_size=0.3, random_state=42
+    df["input"].tolist(), df["label"].tolist(), test_size=0.3, random_state=42
 )
 
 val_texts, test_texts, val_labels, test_labels = train_test_split(
@@ -28,9 +28,9 @@ val_texts, test_texts, val_labels, test_labels = train_test_split(
 # Tokenization
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
-train_encodings = tokenizer(train_texts, truncation=True, padding=True, return_tensors="tf")
-val_encodings = tokenizer(val_texts, truncation=True, padding=True, return_tensors="tf")
-test_encodings = tokenizer(test_texts, truncation=True, padding=True, return_tensors="tf")
+train_encodings = tokenizer(train_texts, truncation=True, padding=True, max_length=128, return_tensors="tf")
+val_encodings = tokenizer(val_texts, truncation=True, padding=True, max_length=128, return_tensors="tf")
+test_encodings = tokenizer(test_texts, truncation=True, padding=True, max_length=128, return_tensors="tf")
 
 # Create Tensorflow Datasets (training and testing)
 train_dataset = tf.data.Dataset.from_tensor_slices((dict(train_encodings), train_labels)).shuffle(1000).batch(8)
@@ -41,10 +41,11 @@ test_dataset = tf.data.Dataset.from_tensor_slices((dict(test_encodings), test_la
 model = TFBertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=2)
 
 # Compile the model
-model.compile(optimizer= tf_keras.optimizers.Adam(learning_rate=5e-5), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=tf_keras.optimizers.Adam(learning_rate=3e-5), loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
 # Training the model
-model.fit(train_dataset, validation_data=val_dataset, epochs=5)
+model.fit(train_dataset, validation_data=val_dataset, epochs=3)
 
 # Save Model
 model.save_pretrained("./fake-news-model-tf")
